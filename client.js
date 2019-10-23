f55666e560254994232f20caca9397350e47fdfa
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-23 22:21:19 +0000

    client modified to use constants for IP and port

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.88.149',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@
 const IP = '192.168.88.149';
 const PORT = 50541;
 
-modules.export = { 
+module.exports = { 
   IP,
   PORT
 };
