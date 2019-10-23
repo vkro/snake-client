6eb88df50e82127997a85b1c3da9f44335d721c7
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-23 20:25:23 +0000

    console.log message on successful connection to game server

--- client.js
+++ client.js
@@ -10,9 +10,13 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
-   conn.on('data', (data) => {
-   console.log(('Server says: '), data);
- })
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
 
   return conn;
 }
