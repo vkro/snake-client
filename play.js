db8565c789db645a0a1fbf7f597f9bac719c06d1
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-23 21:00:39 +0000

    play.js listens for user input, handles ctrl-c

--- play.js
+++ play.js
@@ -3,3 +3,29 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+
+/*
+* "data callback handler for stdin"
+*/
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+}
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', key => handleUserInput(key));
+  return stdin;
+}
+
+setupInput();
